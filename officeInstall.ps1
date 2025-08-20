fcc67053b2ce2cf73aa597bba751a3824fb60795
Gameshler <84982733+gameshler@users.noreply.github.com>
2025-08-20 21:55:48 +0300

    Update officeInstall.ps1

--- officeInstall.ps1
+++ officeInstall.ps1
@@ -42,7 +42,7 @@ function Get-FileFromWeb {
 }
 
 function Install-Office{
-                Clear-Host
+               Clear-Host
                 Write-Host "Installing: Microsoft Office 2024 LSTC Edition . . ."
                 $toolPath = "$env:TEMP\officedeploymenttool_19029-20136.exe"
                 $configurationPath = "$env:TEMP\OfficeDeployment\configuration-Office365-x64.xml"
@@ -81,25 +81,21 @@ function Install-Office{
                 Start-Process -FilePath "$env:TEMP\OfficeDeployment\setup.exe" -ArgumentList "/configure $configurationPath" -Wait
                 # activating microsoft office
                 Write-Host "Activating Microsoft Office . . ."
-                $officePaths = @(
-                    "$env:ProgramFiles(x86)\Microsoft Office\Office16",
-                    "$env:ProgramFiles\Microsoft Office\Office16"
-                )
-
-                foreach ($path in $officePaths) {
-                    if (Test-Path $path) {
-                        Set-Location -Path $path
-                        $licenseFiles = Get-ChildItem -Path "..\root\Licenses16\ProPlus2021VL_KMS*.xrm-ms"
-                        foreach ($file in $licenseFiles) {
-                            Start-Process -FilePath "cscript.exe" -ArgumentList "ospp.vbs /inslic:`"$file`"" -Wait
-                        }
-                        Start-Process -FilePath "cscript.exe" -ArgumentList "ospp.vbs /setprt:1688" -Wait
-                        Start-Process -FilePath "cscript.exe" -ArgumentList "ospp.vbs /unpkey:6F7TH >nul" -Wait
-                        Start-Process -FilePath "cscript.exe" -ArgumentList "ospp.vbs /inpkey:FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH" -Wait
-                        Start-Process -FilePath "cscript.exe" -ArgumentList "ospp.vbs /sethst:107.175.77.7" -Wait
-                        Start-Process -FilePath "cscript.exe" -ArgumentList "ospp.vbs /act" -Wait
-                    }
-                }
+                $cmdScript = @"
+@echo off
+cd /d "%ProgramFiles(x86)%\Microsoft Office\Office16"
+cd /d "%ProgramFiles%\Microsoft Office\Office16"
+for /f %x in ('dir /b ..\root\Licenses16\ProPlus2021VL_KMS*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%x"
+cscript ospp.vbs /setprt:1688
+cscript ospp.vbs /unpkey:6F7TH >nul
+cscript ospp.vbs /inpkey:FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH
+cscript ospp.vbs /sethst:23.226.136.46
+cscript ospp.vbs /act
+pause
+"@
+$scriptPath = Join-Path $env:TEMP "activate.cmd"
+$cmdScript | Set-Content -Path $scriptPath -Encoding ASCII
+Start-Process cmd.exe -ArgumentList "/c", $scriptPath -Verb RunAs
 }
 
 Install-Office
